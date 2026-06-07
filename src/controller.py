fe016857439050f06229424cf7e398e1474cef92
lk14 <leon.kauk@web.de>
2026-06-07 13:35:33 +0200

    Update controller

---
+++ src/controller.py
@@ -0,0 +1,13 @@
+from time import sleep
+import threading
+
+FLOW_RATE = {
+    "aperol": 90,
+    "prosecco": 90, 
+    "soda": 90,
+    "limoncello": 90
+}
+
+
+def pout
+    
---
+++ src/recipes.py
@@ -0,0 +1,11 @@
+APEROL_SPRITZ = {
+    "aperol": 60,
+    "prosecco": 90,
+    "soda": 30
+}
+
+LIMONCELLO_SPRITZ = {
+    "limoncello": 60,
+    "prosecco": 90,
+    "soda": 30
+}
